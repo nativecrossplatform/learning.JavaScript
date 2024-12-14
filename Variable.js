ab53f8ba290372d7e0684c3d08e4b1979666a6ae
Jash Upadhyay <jashupadhyay.work@gmail.com>
2024-12-14 21:35:04 +0530

    Variables File Updated

---
+++ Variable.js
@@ -0,0 +1,8 @@
+var person; //declaring a variable
+person = 'John'; //initializing the variable
+console.log("Hello,",person); //trying to use the variable
+var greeting; 
+greeting = 'Hello,';
+console.log(greeting,person);
+person = "James"; //reassignment
+console.log(greeting,person);
