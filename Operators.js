63d5edbb216e2a05d60b9197b4dbb851cf7532c9
Jash Upadhyay <jashupadhyay.work@gmail.com>
2024-12-15 00:33:18 +0530

    Operators Updated

--- Operators.js
+++ Operators.js
@@ -1,5 +1,28 @@
 /*
+
+Basic Operators:
+
 -> Assignment Operator (=)
 Used to Assign a value to something.
+
+-> Arithmetic operators ( + , - , * , / , %):
++ -> Addition
+- -> Subtraction
+* -> Multiplication
+/ -> Division
+% -> Modulus
+
+-> Comparison Operators:
+(used to compare between two entities)
+>= greater than equal to
+<= less than equal to
+>  greater than
+<  less than
+!= not equal to 
+
+-> Logical Operators:
+&& AND
+|| OR
+!  NOT
 
 */
