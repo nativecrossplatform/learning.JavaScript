46f6fe94e06b1f84e485fa76819fad661ebdfa99
Jash Upadhyay <jashupadhyay.work@gmail.com>
2024-12-15 00:45:48 +0530

    Operator FileUpdate

--- Operators.js
+++ Operators.js
@@ -7,18 +7,27 @@ Used to Assign a value to something.
 
 -> Arithmetic operators ( + , - , * , / , %):
 + -> Addition
+( when used to combine a string and a string, the addition operator acts as a concatenation operator and joins the two strings
+if used on a number and a string then it will treat the number as a string and then add it for eg: 12 + "hello" will give 12hello as output)
 - -> Subtraction
 * -> Multiplication
 / -> Division
 % -> Modulus
++= Addition Assignment
 
 -> Comparison Operators:
 (used to compare between two entities)
->= greater than equal to
-<= less than equal to
->  greater than
-<  less than
-!= not equal to 
+>= Greater than equal to
+<= Less than equal to
+>  Greater than
+<  Less than
+!= Not equal to 
+== Equal to
+=== Strictly equal to
+(The strict equality operator compares for both the values and the data types. With the strict equality operator, comparing 5 === 5 still returns true. The values on each side of the strict equality operator have the same value and the same type. However, comparing 5 == "5" now returns false, because the values are equal, but the data type is different.)
+!== Strictly not equal to
+(For the strict inequality operator to return false, the compared values have to have the same value and the same data type. 
+For example,  5 !== 5 returns false because it is false to say that the number 5 is not of the same value and data type and another number 5.)
 
 -> Logical Operators:
 && AND
