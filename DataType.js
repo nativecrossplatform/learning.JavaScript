666e20ca20490be52dc0c85a70e3e68db54fdc65
Jash Upadhyay <jashupadhyay.work@gmail.com>
2024-12-14 21:53:29 +0530

    Data Types File Added

---
+++ DataType.js
@@ -0,0 +1,17 @@
+/*
+Seven Primitive DataTypes Are:
+1. String
+Text data type is reffered to as string.
+2. Number
+Numeric data type is reffered to as Number.
+3. Boolean
+True/False values only.
+4. Null
+Absence of a value.
+5. Undefined
+A variable is not yet assigned value.
+6. Bigint
+A data type that accomodates a greater range of numbers
+7. Symbol
+A data type used as an unique identifier.
+*/
