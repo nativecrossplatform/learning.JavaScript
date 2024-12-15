a4ed7f56683331c580d8b74618085cc6629de4e6
Jash Upadhyay <jashupadhyay.work@gmail.com>
2024-12-15 10:57:39 +0530

    Objects And Arrays

--- Array.js
+++ Array.js
@@ -8,3 +8,8 @@ for(var i=0;i<arr.length;i++)
 {
     console.log(arr[i]);
 }
+// Arrays are objects too
+arr.push("Pineapple");
+console.log(arr);
+arr.pop(); // removes the element last added
+console.log(arr);
--- Objects.js
+++ Objects.js
@@ -14,12 +14,13 @@ car.cost = 70000000;
 console.log(car);
 
 // Alternate Method
-var bike = {maker:"Yamaha",health:100}
+var bike = {maker:"Yamaha",health:100};
+bike["cost"] = 800000;
 
 // checking the value
 console.log(bike);
 
-// To acess a particular property of the object we do not use index but rather use the key value to access it. for eg if i want to access the maker of the object bike i can access it using the way given below
+// To acess a particular property of the object we do not use index but rather use the key value to access it. for eg if i want to access the maker of the object bike i can access it using these ways given below
 
 console.log(bike.maker);
-
+console.log(bike["maker"]);
