d038ac4068e1cc1d778a8396f0533f2bbdc266bc
Jash Upadhyay <jashupadhyay.work@gmail.com>
2024-12-15 11:30:35 +0530

    typeof x

---
+++ TypeOf.js
@@ -0,0 +1,13 @@
+console.log("typeof undefined:", typeof undefined);
+console.log("typeof null:", typeof null);
+console.log("typeof true:", typeof true);
+console.log("typeof 42:", typeof 42);
+console.log("typeof 'Hello':", typeof "Hello");
+console.log("typeof Symbol('example'):", typeof Symbol("example"));
+console.log("typeof function() {}:", typeof function () {});
+console.log("typeof []:", typeof []);
+console.log("typeof {}:", typeof {});
+console.log("typeof /regex/:", typeof /regex/);
+console.log("typeof new Date():", typeof new Date());
+console.log("typeof BigInt(123):", typeof BigInt(123));
+ 
