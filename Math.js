5ae623c24183fabfba4ac9542da3465b3e2e1203
Jash Upadhyay <jashupadhyay.work@gmail.com>
2024-12-15 11:19:13 +0530

    Math File Added

---
+++ Math.js
@@ -0,0 +1,43 @@
+// CONSTANTS
+
+console.log("----------Constants----------")
+// PI
+console.log(Math.PI);
+// Euler's Constant
+console.log(Math.E);
+// Natural Logrithm Of 2
+console.log(Math.LN2);
+// Natural Logarithm Of 10
+console.log(Math.LN10);
+
+// Rounding Methods
+
+console.log("----------Rounding Methods----------")
+console.log("----------CEIL----------")
+console.log(Math.ceil(10.333))
+console.log(Math.ceil(10.500))
+console.log(Math.ceil(10.555))
+console.log("----------FLOOR----------")
+console.log(Math.floor(10.333))
+console.log(Math.floor(10.500))
+console.log(Math.floor(10.555))
+console.log("----------ROUND----------")
+console.log(Math.round(10.333))
+console.log(Math.round(10.500))
+console.log(Math.round(10.555))
+console.log("----------TRUNC----------")
+console.log(Math.trunc(10.333))
+console.log(Math.trunc(10.500))
+console.log(Math.trunc(10.555))
+
+// ARITHMETIC AND CALCULUS METHODS
+
+console.log("----------ARITHMETIC AND CALCULUS METHODS----------")
+console.log(Math.pow(2,4));
+console.log(Math.sqrt(25));
+console.log(Math.cbrt(125)); // cube root 
+console.log(Math.abs(-100)); // Absolute Value [ Mathematical Modulus Function ]
+
+// Trigonometric functions like Math.sin(),Math.cos(),Math.tan() etc also exist.
+
+// Lograithmic functions like Math.log(), Math.log2(), Math.log10() are also available
