bce7fcdc16feb96d24f87e6026f69efea5bbfaa3
Jash Upadhyay <jashupadhyay.work@gmail.com>
2024-12-15 09:37:48 +0530

    Miscl Update

---
+++ Array.js
@@ -0,0 +1,10 @@
+// Declaring an Array
+var arr = [];
+// Initializing an Array
+arr = ["Apple","Banana","Mango"];
+console.log(typeof(arr));
+// Accessing elements with the help of index
+for(var i=0;i<arr.length;i++)
+{
+    console.log(arr[i]);
+}
---
+++ Objects.js
@@ -0,0 +1,25 @@
+// OBJECTS
+
+// Declaring an object
+var car = {};
+
+//assigning properties
+car.health = 100;
+car.type = "Sedan";
+car.model = "Phantom";
+car.maker = "Rolls-Royce";
+car.cost = 70000000;
+
+// checking the value
+console.log(car);
+
+// Alternate Method
+var bike = {maker:"Yamaha",health:100}
+
+// checking the value
+console.log(bike);
+
+// To acess a particular property of the object we do not use index but rather use the key value to access it. for eg if i want to access the maker of the object bike i can access it using the way given below
+
+console.log(bike.maker);
+
