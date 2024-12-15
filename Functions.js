4944f21ea0201a42812ca46583c5c477c4d706dc
Jash Upadhyay <jashupadhyay.work@gmail.com>
2024-12-15 08:58:45 +0530

    FUnctions And Arrays File Added

---
+++ Functions.js
@@ -0,0 +1,16 @@
+// FUNCTIONS
+
+/*
+function function_name()
+{
+    EXECUTE;
+}
+*/
+function add(a,b) // function definition
+{
+    return a+b;
+}
+for(var i=1;i<=5;i++)
+{
+    console.log(add(i,i+1)); // function call
+}
