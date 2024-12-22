3b9229047be834b05d92b05bb836e6f211019980
Jash Upadhyay <jashupadhyay.work@gmail.com>
2024-12-22 15:45:53 +0530

    uen2

--- Undefined.Empty.Null.js
+++ Undefined.Empty.Null.js
@@ -1,6 +1,6 @@
 //NULL
 /* 
-Null: Intentional Abscence of any object value
+Null: Abscence of any object value
 also return value for many built in javascript methods
 */
 var str = 'abc';
@@ -8,7 +8,13 @@ console.log(str.match(/a/)); // returns relevant details
 console.log(str.match(/z/)); // returns null
 //UNDEFINED
 /*
+-> A DATA TYPE WHICH CAN HAVE ONLY ONE POSSIBLE VALUE THAT IS Undefined
+-> APPEARS WHEN VARIABLE IS DECLARED WITHOUT ASSIGNMENT
 */
+var noise; //best practice to assign with declaration
+console.log(noise);
+noise = 'aaa';
+console.log(noise);
 //EMPTY
 /*
 */
