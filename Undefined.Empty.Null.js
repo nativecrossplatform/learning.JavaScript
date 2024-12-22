bcf9241ffae562b007d097147cbd21b1654183cb
Jash Upadhyay <jashupadhyay.work@gmail.com>
2024-12-22 15:34:29 +0530

    uen

---
+++ Undefined.Empty.Null.js
@@ -0,0 +1,14 @@
+//NULL
+/* 
+Null: Intentional Abscence of any object value
+also return value for many built in javascript methods
+*/
+var str = 'abc';
+console.log(str.match(/a/)); // returns relevant details
+console.log(str.match(/z/)); // returns null
+//UNDEFINED
+/*
+*/
+//EMPTY
+/*
+*/
