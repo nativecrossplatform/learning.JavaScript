4834c0c1e2f12dd22588f16fb028995b75a63a75
Jash Upadhyay <jashupadhyay.work@gmail.com>
2024-12-23 23:29:02 +0530

    addFive.js Final Commit;

--- addFive.js
+++ addFive.js
@@ -1,6 +1,5 @@
-/*     JEST     */
 function addFive(val)
 {
     return val+5;
 }
-module.exports = addFive;
+module.exports = addFive; // module.exports defines what functions will be exposed when file is used as import for use
