f144ef3971d82280c31bf7a0a9eb296ae0934245
Jash Upadhyay <jashupadhyay.work@gmail.com>
2024-12-23 23:43:14 +0530

    Conditional Statements.js Final Commit;

--- Conditional.Stateents.js
+++ Conditional.Stateents.js
@@ -1,86 +1,154 @@
 // IF STATEMENT
+// The if statement executes a block of code if a specified condition is true.
 /*
-if(condition == true)
-{
-        EXECUTE
+if (condition == true) {
+    EXECUTE
 }
 */
 var jash = 1;
-if(jash == 1)
-{
-    console.log("Yes");
+if (jash == 1) {
+    console.log("Yes"); // Output: Yes
 }
+
 // IF ELSE STATEMENT
+// The if...else statement executes one block of code if the condition is true, and another if it's false.
 /*
-if(condition==true)
-{
-    EXCUTE THIS
-}
-else
-{
+if (condition == true) {
+    EXECUTE THIS
+} else {
     EXECUTE THIS
 }
 */
-if(jash >= 1)
-{
-    console.log('Yes');
-}
-else
-{
+if (jash >= 1) {
+    console.log('Yes'); // Output: Yes
+} else {
     console.log('No');
 }
+
 // ELSE IF STATEMENT
+// The if...else if statement allows you to check multiple conditions.
 /*
-if(condition==true)
-{
-    EXCUTE THIS
-}
-else if(condition2 == true)
-{
+if (condition == true) {
     EXECUTE THIS
-}
-else
-{
+} else if (condition2 == true) {
+    EXECUTE THIS
+} else {
     EXECUTE THIS
 }
 */
 jash = -1;
-if(jash >= 1)
-    {
-        console.log('Yes');
-    }
-else if(jash == -1)
-    {
-        console.log("Good");
-    }
-else
-    {
-        console.log('No');
-    }
+if (jash >= 1) {
+    console.log('Yes');
+} else if (jash == -1) {
+    console.log("Good"); // Output: Good
+} else {
+    console.log('No');
+}
+
 // THERE CAN BE MULTIPLE ELSE IF STATEMENTS
+
 // SWITCH STATEMENTS
+// The switch statement evaluates an expression and executes the matching case.
 /*
-switch(test)
-{
+switch (test) {
     case test-value-1:
         EXECUTE;
         break;
-    case test-value-1:
+    case test-value-2:
         EXECUTE;
         break;
     // SO ON AND SO FORTH
     default:
         EXECUTE;
-        break;  
+        break;
 }
 */
 jash = 0;
-switch(jash)
-{
+switch (jash) {
     case 1:
         console.log('Mumbai');
+        break;
     case -1:
         console.log('Udaipur');
+        break;
     default:
-        console.log('Melbourne');
+        console.log('Melbourne'); // Output: Melbourne
+        break;
+}
+// IF STATEMENT
+// The if statement executes a block of code if a specified condition is true.
+/*
+if (condition == true) {
+    EXECUTE
+}
+*/
+var jash = 1;
+if (jash == 1) {
+    console.log("Yes"); // Output: Yes
+}
+
+// IF ELSE STATEMENT
+// The if...else statement executes one block of code if the condition is true, and another if it's false.
+/*
+if (condition == true) {
+    EXECUTE THIS
+} else {
+    EXECUTE THIS
+}
+*/
+if (jash >= 1) {
+    console.log('Yes'); // Output: Yes
+} else {
+    console.log('No');
+}
+
+// ELSE IF STATEMENT
+// The if...else if statement allows you to check multiple conditions.
+/*
+if (condition == true) {
+    EXECUTE THIS
+} else if (condition2 == true) {
+    EXECUTE THIS
+} else {
+    EXECUTE THIS
+}
+*/
+jash = -1;
+if (jash >= 1) {
+    console.log('Yes');
+} else if (jash == -1) {
+    console.log("Good"); // Output: Good
+} else {
+    console.log('No');
+}
+
+// THERE CAN BE MULTIPLE ELSE IF STATEMENTS
+
+// SWITCH STATEMENTS
+// The switch statement evaluates an expression and executes the matching case.
+/*
+switch (test) {
+    case test-value-1:
+        EXECUTE;
+        break;
+    case test-value-2:
+        EXECUTE;
+        break;
+    // SO ON AND SO FORTH
+    default:
+        EXECUTE;
+        break;
+}
+*/
+jash = 0;
+switch (jash) {
+    case 1:
+        console.log('Mumbai');
+        break;
+    case -1:
+        console.log('Udaipur');
+        break;
+    default:
+        console.log('Melbourne'); // Output: Melbourne
+        break;
 }
